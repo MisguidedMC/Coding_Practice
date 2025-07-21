e8eb65b5678111928ac73a335afd177cf0e1d2d2
MisguidedMC <pranaydeepsingh.123@gmail.com>
2025-07-21 14:14:49 +1200

    Java_Numbers.java

---
+++ Java_Numbers.java
@@ -0,0 +1,27 @@
+public class Java_Numbers{
+    public static void main(String[] args) {
+        
+        //This lesson focuses on Different types of java varible tyes
+        // that holds numbers
+
+        // byte holds numbers from -128 - 128
+
+
+        byte byteNum = 69;
+        System.out.println(byteNum);
+
+        // short holds numberss from -32768 to 32767
+
+        short shortNum = 690;
+        System.out.println(shortNum);
+
+        // int holds numbers from -2147483648 to 2147483647
+        int intNum=69420;
+        System.out.println(intNum);
+
+        // Long holds numbers from  from -9223372036854775808 to 9223372036854775807
+        // long is ended with a L
+        long longNum = 420000000069L;
+        System.out.println(longNum);
+    }
+}
