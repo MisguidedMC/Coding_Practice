1a8ba086ea5d16125084b30974f32a908ba47e9b
MisguidedMC <pranaydeepsingh.123@gmail.com>
2025-07-21 14:32:18 +1200

    Java_Characters.Java

---
+++ Java_Characters.java
@@ -0,0 +1,18 @@
+public class Java_Characters{
+    public static void main(String[] args) {
+        //This lesson includes types of Java Characters
+        
+
+        //Char -stores a single character
+
+        char nameIntial = 'P';
+        System.out.println("Your Name's intial is " + nameIntial);
+
+        //String - stores sequence of characters
+        
+        String praise = "Good Work!";
+        System.out.println("");
+        System.out.println(praise);
+        
+    }
+}
